50af856b85b1c6505cd46f94b2d818a9fb09f14e
mainuzzamanbabu <mainuzzamanb@gmail.com>
2023-02-06 17:38:54 +0600

    First Commit

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import utilStyles from '../styles/utils.module.css'
+// pages/404.js
+export default function Custom404() {
+    return <h1 className={utilStyles.headingLg}>404 - Page Not Found</h1>;
+}
